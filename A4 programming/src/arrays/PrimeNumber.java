9e5351375b0bfa228404662b1a4dfeb99dec3b61
Praveen M Ghali <Praveen M Ghali@LAPTOP-RL7JVSAU>
2025-03-04 21:06:21 +0530

    Praveen 2nd commit

--- A4 programming/src/arrays/PrimeNumber.java
+++ A4 programming/src/arrays/PrimeNumber.java
@@ -3,8 +3,7 @@ package arrays;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("This is Prime number program");
 	}
 
 }
